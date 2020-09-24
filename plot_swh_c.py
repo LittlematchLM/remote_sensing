770d51d6acd4cc86ae033abcd4d73fae88e57155
LittlematchLM <529289524@qq.com>
2020-09-24 14:56:22 +0800

    200924修改了plot的备注

--- plot_swh_c.py
+++ plot_swh_c.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 from netCDF4 import Dataset
 import numpy as np
 
-fig = plt.figure()
+"读取单个netCDF文件并可视化，最后图像为一条轨道上的数据"
 
+fig = plt.figure()
+# read netCDF file
 my_ncfile = 'E:\python_workfile\\remote_sensing_alt_demo\H2B_nc_data\H2B_OPER_GDR_2PC_0043_0082_20200611T183611_20200611T192819.nc'
 fh = Dataset(my_ncfile, mode='r')
 
@@ -16,9 +18,10 @@ fh.close()
 lon_mean = lons.mean()
 lat_mean = lats.mean()
 
+# Draw the map
 m = Basemap(projection='cyl', llcrnrlat=-90, urcrnrlat=90, llcrnrlon=0, urcrnrlon=360, resolution='c')
 x, y = m(lons, lats)
-
+# Draw the scatterplot
 m.scatter(x, y, c=swhc, marker='.', cmap=plt.cm.jet, alpha=0.8)
 m.colorbar(location='right')
 
