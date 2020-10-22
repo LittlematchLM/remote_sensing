e2e93ee4a65e64e5d7e60a66771cd86c813352ed
LittlematchLM <529289524@qq.com>
2020-10-22 20:27:42 +0800

    201022 极方位角等距投影完成

--- temp.py
+++ temp.py
@@ -6,6 +6,7 @@ y = np.arange(-5, 4, 1)
 xx, yy = np.meshgrid(x, y)
 z = np.sin(xx ** 2 + yy ** 2) / (xx ** 2 + yy ** 2)
 z[0, 0] = np.nan
+z[3, 1] = np.nan
 h = plt.pcolor(x, y, z)
 
 print(x.shape, y.shape,)
