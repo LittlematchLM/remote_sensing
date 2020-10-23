4d6b08384e875437f2d50887134b00d841586a35
LittlematchLM <529289524@qq.com>
2020-10-23 15:35:53 +0800

    201023 cryosat2极方位角等距投影完成

--- plot_day_oneimg.py
+++ plot_day_oneimg.py
@@ -7,50 +7,43 @@ import glob
 
 "读取一个文件夹的n个netCDF文件并可视化，最后图像为n条轨道上的数据"
 
-
-def read_ncfile_value(fh, value):
-    '''读取文件，value为要读取的参数
-    return： lons, lats, value'''
-    lons = fh.variables['lon'][:]
-    lats = fh.variables['lat'][:]
-    value = fh.variables[value][:]
-    return lons, lats, value
-
-
 fig = plt.figure(figsize=(16, 12))
 # read all netCDF file
-dir_path = 'E:\python_workfile\\remote_sensing\H2B_nc_data\H2B_20200711'
-ncfiles = glob.glob(dir_path + '\*.nc')
-lon_array = np.array([])
-lat_array = np.array([])
-swhc_array = np.array([])
-swhc_mask_array = np.array([])
-for ncfile in ncfiles:
-    fh = Dataset(ncfile, mode='r')
-    lons = fh.variables['lon'][:]
-    lats = fh.variables['lat'][:]
-    swhc = fh.variables['swh_c'][:]
-    swhc_mask = swhc.mask
-    lon_array = np.append(lon_array, lons)
-    lat_array = np.append(lat_array, lats)
-    swhc_array = np.append(swhc_array, swhc)
-    swhc_mask_array = np.append(swhc_mask_array, swhc_mask)
-    fh.close()
+cy_dir_path = 'F:\\remote_sensing_data\CRYOSAT-2\\20200711'
+cyfiles = glob.glob(cy_dir_path + '\*.nc')
+cy_lon_array = np.array([])
+cy_lat_array = np.array([])
+cy_swh_array = np.array([])
+cy_swh_mask_array = np.array([])
+for cyfile in cyfiles:
+    with Dataset(cyfile, mode='r') as fh:
+        lons = fh.variables['lon_01'][:]
+        lats = fh.variables['lat_01'][:]
+        swh = fh.variables['swh_ocean_01_ku'][:]
+        swh_mask = swh.mask
+        if np.array(swh_mask, dtype=bool).shape == ():
+            swh_mask = np.full((len(swh),), False)
+        cy_lon_array = np.append(cy_lon_array, lons)
+        cy_lat_array = np.append(cy_lat_array, lats)
+        cy_swh_array = np.append(cy_swh_array, swh)
+        cy_swh_mask_array = np.append(cy_swh_mask_array, swh_mask)
 
-m = Basemap(projection='cyl', llcrnrlat=-90, urcrnrlat=90, llcrnrlon=0, urcrnrlon=360, resolution='c')
-#m = Basemap(projection='npaeqd', boundinglat=60, lon_0=180, resolution='c')
-x, y = m(lon_array, lat_array)
+cy_swh_array_masked = np.ma.array(cy_swh_array, mask=cy_swh_mask_array)
+
+#m = Basemap(projection='cyl', llcrnrlat=-90, urcrnrlat=90, llcrnrlon=0, urcrnrlon=360, resolution='c')
+m = Basemap(projection='npaeqd', boundinglat=60, lon_0=180, resolution='c')
+x, y = m(cy_lon_array, cy_lat_array)
 
 # Draw the scatterplot
-swhc_array_masked = np.ma.array(swhc_array, mask=swhc_mask_array)
-h = m.scatter(x, y, c=swhc_array_masked, marker='o', cmap=plt.cm.jet, alpha=0.8)
+
+h = m.scatter(x, y, c=cy_swh_array_masked, marker='o', cmap=plt.cm.jet, alpha=0.8)
 m.colorbar(location='right')
 m.drawcoastlines()
 m.fillcontinents()
 m.drawmapboundary()
 m.drawparallels(np.arange(-90., 120., 30.), labels=[1, 0, 0, 0])
 m.drawmeridians(np.arange(-180., 180., 30.), labels=[0, 0, 0, 1])
-plt.cm.ScalarMappable.set_clim(h, vmin=0, vmax=10)
+plt.cm.ScalarMappable.set_clim(h, vmin=0, vmax=5)
 # 测试宿舍电脑上传
 plt.show()
 # 存储图像
