604d272936f75dd86eab7eba529fdc5e5cefad62
LittlematchLM <529289524@qq.com>
2020-10-27 19:58:34 +0800

    201027 对比重合点差值工作中

--- temp.py
+++ temp.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(-5, 5, 1)
+'''x = np.arange(-5, 5, 1)
 y = np.arange(-5, 4, 1)
 xx, yy = np.meshgrid(x, y)
 z = np.sin(xx ** 2 + yy ** 2) / (xx ** 2 + yy ** 2)
@@ -12,3 +12,9 @@ h = plt.pcolor(x, y, z)
 print(x.shape, y.shape,)
 print(xx.shape, yy.shape, z.shape)
 plt.show()
+'''
+
+a = np.array([np.nan, 1, 3, np.nan, np.nan, 0.6]).reshape(2, 3)
+b = np.array([np.nan, np.nan, 2.7, 0.7, 0.5, np.nan]).reshape(2, 3)
+
+
