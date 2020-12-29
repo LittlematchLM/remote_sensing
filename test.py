2b94bc2f45e53a98681c12b5206669a988134f54
LittlematchLM <529289524@qq.com>
2020-12-29 19:24:28 +0800

    20201229 时间窗口平均

--- test.py
+++ test.py
@@ -1,83 +1,60 @@
-from HaiYangData import *
-import glob
+import numpy as np
+import pandas as pd
+hy_cy_coincident_list = np.array([['750+38', 619631968.508203, 1.37, 619632901.375125, 0.984],
+ ['751+38', 619631969.516053, 1.252, 619632899.488252, 1.284],
+ ['751+38', 619631969.516053, 1.252, 619632900.431687, 1.564],
+ ['752+38', 619631970.498717, 1.216, 619632898.544823, 1.207],
+ ['754+15', 620496875.487952, 1.717, 620496159.649289, 1.896],
+ ['754+15', 620496875.487952, 1.717, 620496160.592718, 1.924],
+ ['755+15', 620496876.49617, 1.82, 620496158.705851, 1.793],
+ ['755+-120', 620503126.503192, 2.648, 620502126.6794109, 2.652],
+ ['755+-120', 620503127.511161, 2.468, 620502126.6794109, 2.652],
+ ['756+-120', 620503128.51914, 2.29, 620502125.73597, 2.574],
+ ['756+-120', 620503128.51914, 2.7, 620502125.73597, 2.896],
+ ['756+-120',620505128,3.12,620503333,3.22]
+ ])
 
+time_win = 1800
+hy_cy_df = pd.DataFrame(hy_cy_coincident_list, columns=['ij', 'hy_time', 'hy_value', 'other_time', 'other_value'])
 
-hyfiles = glob.glob(r'G:\remote_sensing_data\hy2b\ALT\2019\07\*.nc')
-hyfiles = hyfiles[0:28]
-hy_value = ['swh_ku','swh_c','sig0_ku']
+hy_cy_df['hy_time_round'] = ((hy_cy_df['hy_time'].astype(np.float))/time_win).astype(np.int)
 
-hy_alt = HaiYangData(satellite='hy', sensor='alt',resolution=25000)
-hy_ori_df = pd.DataFrame(np.column_stack((hy_alt.from_nc_files(hyfiles, value=hy_value))), columns=['lon', 'lat', 'time']+hy_value)
+hy_cy_df['other_time_round'] = ((hy_cy_df['other_time'].astype(np.float))/time_win).astype(np.int)
+hy_cy_mean_df = hy_cy_df.groupby(['ij','hy_time_round','other_time_round']).mean()
 
-# 删除无效点,只处理北纬66°以上的数据
-hy_fill_value = 32767
-hy_ori_df = hy_ori_df.drop(list(np.where(hy_ori_df['swh_ku'] == hy_fill_value)[0][:]))
-hy_ori_df = hy_alt.data_filter(hy_ori_df,'lat',66)
+hy_cy_df
 
-# 将WGS 84坐标（4326）转化为极射投影
-crs = CRS.from_epsg(4326)
-crs = CRS.from_string("epsg:4326")
-crs = CRS.from_proj4("+proj=latlon")
-crs = CRS.from_user_input(4326)
-crs2 = CRS(proj="aeqd")
+#
+# c = pd.DataFrame(columns=['ij', 'hy_time', 'hy_value', 'other_time', 'other_value'])
+#
+# b = np.array([])
+# time1, time2, value1, value2, num_count = 0, 0, 0, 0, 0
+# for num in range(1,hy_cy_coincident_list.shape[0]):
+#
+#     if hy_cy_coincident_list[num-1][0] == hy_cy_coincident_list[num][0]:
+#
+#         if((float(hy_cy_coincident_list[num-1][1]) - float(hy_cy_coincident_list[num][1]))< time_win) and ((float(hy_cy_coincident_list[num-1][3]) - float(hy_cy_coincident_list[num][3]))< time_win):
+#             value1 += np.float(hy_cy_coincident_list[num-1][2])
+#             time1 +=  np.float(hy_cy_coincident_list[num-1][1])
+#             value2 += np.float(hy_cy_coincident_list[num-1][4])
+#             time2 += np.float(hy_cy_coincident_list[num-1][3])
+#             num_count += 1
+#             print(num_count)
+#             print(value1,time1,value2,time2)
+#         else:
+#             value1 += np.float(hy_cy_coincident_list[num-1][2])
+#             time1 +=  np.float(hy_cy_coincident_list[num-1][1])
+#             value2 += np.float(hy_cy_coincident_list[num-1][4])
+#             time2 += np.float(hy_cy_coincident_list[num-1][3])
+#             num_count += 1
+#             temp = np.array([hy_cy_coincident_list[num-1][0],time1/num_count,value1/num_count,time2/num_count,value2/num_count])
+#             print('该停止了')
+#             np.hstack(b,temp)
+#             time1, time2, value1, value2, num_count = 0, 0, 0, 0, 0
+#     else:
+#         print('下一个点')
+#         b = np.hstack((b,hy_cy_coincident_list[num-1]))
+#
+#
+# b = b.reshape(6,5)
 
-transformer = hy_alt.transforme(crs,crs2)
-transformer_back = hy_alt.transforme(crs2,crs)
-
-df_e,df_w = hy_alt.split_W_E_earth(hy_ori_df,transformer)
-
-# 交叉点平均化
-mean_grid_e = hy_alt.coincident_point_mean(df_e,'swh_ku',hy_alt.nlat, hy_alt.nlon,hy_alt.resolution)
-mean_grid_w = hy_alt.coincident_point_mean(df_w,'swh_ku',hy_alt.nlat, hy_alt.nlon,hy_alt.resolution)
-
-
-finally_grid = np.full(shape=(hy_alt.nlat,hy_alt.nlon),fill_value=65533)
-finally_grid[np.where((np.isnan(mean_grid_e)) == False)] = mean_grid_e[np.where((np.isnan(mean_grid_e)) == False)]
-finally_grid[np.where((np.isnan(mean_grid_w)) == False)] = mean_grid_w[np.where((np.isnan(mean_grid_w)) == False)]
-
-# 校准y坐标
-hy_x = np.arange(hy_alt.nlat)
-hy_y = np.arange(hy_alt.nlon)
-hy_xx , hy_yy = np.meshgrid(hy_x * hy_alt.resolution, hy_y * hy_alt.resolution)
-
-# 将xx,yy转换回坐标形式
-hy_y_map, hy_x_map = transformer_back.transform(hy_xx , hy_yy)
-
-hy_x_map_west = hy_x_map-180
-
-
-plt.figure(figsize=(16, 9))
-
-hy_m = Basemap(projection='npaeqd', boundinglat=66, lon_0=180, resolution='c')
-# hy_xi, hy_yi = hy_m(hy_x_map, hy_y_map)
-# hy_xi_west ,hy_yi_west = hy_m(hy_x_map_west, hy_y_map_west)
-# Draw the scatterplot
-h = hy_m.pcolormesh(hy_x_map, hy_y_map, data=finally_grid, cmap=plt.cm.jet,vmin=0, vmax=5,latlon = True)
-# h2 = hy_m.pcolormesh(hy_x_map_west1 ,hy_y_map, data=finally_grid, cmap=plt.cm.jet,vmin=0, vmax=5,latlon = True)
-hy_m.colorbar(location='right')
-hy_m.drawcoastlines()
-hy_m.fillcontinents()
-hy_m.drawmapboundary()
-hy_m.drawparallels(np.arange(-90., 120., 10.), labels=[1, 0, 0, 0])
-hy_m.drawmeridians(np.arange(-180., 180., 60.), labels=[0, 0, 0, 1])
-# plt.title("HY2B SWH  "+ year+ month)
-plt.show()
-plt.close()
-
-
-plt.figure(figsize=(9,9))
-#m = Basemap(projection='cyl', llcrnrlat=-90, urcrnrlat=90, llcrnrlon=0, urcrnrlon=360, resolution='c')
-m = Basemap(projection='npaeqd', boundinglat=60, lon_0=180, resolution='c')
-x, y = m(hy_ori_df['lon'].values, hy_ori_df['lat'].values)
-
-# Draw the scatterplot
-
-h = m.scatter(hy_ori_df['lon'].values, hy_ori_df['lat'].values, c=hy_ori_df['swh_ku'].values, marker='o', cmap=plt.cm.jet, alpha=0.8,latlon=True)
-m.colorbar(location='right')
-m.drawcoastlines()
-m.fillcontinents()
-m.drawmapboundary()
-m.drawparallels(np.arange(-90., 120., 30.), labels=[1, 0, 0, 0])
-m.drawmeridians(np.arange(-180., 180., 30.), labels=[0, 0, 0, 1])
-plt.cm.ScalarMappable.set_clim(h, vmin=0, vmax=5)
-plt.show()
